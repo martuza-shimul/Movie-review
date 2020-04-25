2ccf73992b8da9c2f1d2118af628b80a80e9b966
Martuza <martuza.shimul@gmail.com>
2020-04-26 05:19:56 +0600

    responsive navbar added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,27 @@
 import React from "react";
+import poweredImg from "../img/powered.png";
 
 const Footer = () => {
 	return (
 		<div>
 			<footer>
-				<div className="credit">
-					<p>
-						Built with{" "}
-						<span role="img" aria-label="">
-							❤️
-						</span>
-						Powered by <a href="themoviedb.org">The MovieDB</a>
-					</p>
-				</div>
-				<div className="copyright">
-					<p>
-						{" "}
-						© 2020, MovieDB -- Developed by{" "}
-						<a href="https://github.com/martuza-shimul">Martuza</a>
-					</p>
+				<div className="container">
+					<div className="copyright">
+						<p>
+							Built with{" "}
+							<span role="img" aria-label="">
+								❤️
+							</span>
+						</p>
+						<p>
+							{" "}
+							© 2020 Lex Go! - Developed by{" "}
+							<a href="https://github.com/martuza-shimul">Martuza</a>
+						</p>
+					</div>
+					<div className="credit">
+						<img src={poweredImg} alt="Powered By" />
+					</div>
 				</div>
 			</footer>
 		</div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,43 @@
 import React from "react";
 
-const Header = () => {
-	return (
-		<div>
-			<header className="header">
-				<nav>
-					<div className="logo">
-						<h5>
-							<a href="/">Movie DB</a>
-						</h5>
-					</div>
-					<ul className="navList">
-						<li className="navItem">
-							<a href="/movie">Movie</a>
-						</li>
-						<li className="navItem">
-							<a href="/tv-series">TV Series</a>
-						</li>
-						<li className="navItem">
-							<a href="/person">Person</a>
-						</li>
-					</ul>
-				</nav>
-			</header>
-		</div>
-	);
-};
+class Header extends React.Component {
+	state = {
+		navOpen: false,
+	};
+	handleNav = () => this.setState({ navOpen: !this.state.navOpen });
+	render() {
+		return (
+			<div>
+				<header className="header">
+					<nav>
+						<div className="logo">
+							<h5>
+								<a href="/">lex Go!</a>
+							</h5>
+						</div>
+						<div className="navIcon" onClick={this.handleNav}>
+							<p></p>
+							<p></p>
+							<p></p>
+						</div>
+						<ul className={this.state.navOpen ? "navList navOpen" : "navList"}>
+							<li onClick={this.handleNav} className="navItem close">
+								<a href="/#">X</a>
+							</li>
+							<li className="navItem">
+								<a href="/movie">Movie</a>
+							</li>
+							<li className="navItem">
+								<a href="/tv-series">TV Series</a>
+							</li>
+							<li className="navItem">
+								<a href="/person">Person</a>
+							</li>
+						</ul>
+					</nav>
+				</header>
+			</div>
+		);
+	}
+}
 export default Header;
